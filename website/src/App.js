d745806fdf3460285fbfdb682b74b3e9466a28ec
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-11-02 17:12:56 -0400

    basic home page

---
+++ website/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Vacant Homes
+        </h1>
+        <p>
+          Find your next investment. Send an offer. All in one platform.
+        </p>
+        <input></input>
+        <button>Search</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
